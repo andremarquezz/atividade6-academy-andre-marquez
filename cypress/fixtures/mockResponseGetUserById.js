48e58dfd957560c7c672d14a4be2ed5f92465b0d
Jey <andre.marqquez@gmail.com>
2024-05-04 11:23:22 -0300

    add: mocks

---
+++ cypress/fixtures/mockResponseGetUserById.js
@@ -0,0 +1,7 @@
+export const mockResponseGetUserById = {
+  id: "dfc10878-a1ac-4a0c-a4f6-6b2984d77ac2",
+  name: "Jey",
+  email: "Jey@qa.com",
+  createdAt: "2024-04-25T19:31:42.421Z",
+  updatedAt: "2024-04-25T19:31:42.421Z",
+};
---
+++ cypress/fixtures/mocksErrors.js
@@ -0,0 +1,9 @@
+export const mockErrorUserAlreadyExists = {
+  statusCode: 422,
+  error: "User already exists.",
+};
+
+export const mockErrorInternalServer = {
+  statusCode: 500,
+  error: "Internal Server Error",
+};
