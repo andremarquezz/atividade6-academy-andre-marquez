7998499664820d6e5d11a39e034cc76e097ef3b8
Andre Marquez <andre.marqquez@gmail.com>
2024-05-08 09:49:13 -0300

    add: command createMultipleUsers e createSingleUser

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,23 @@
 import { faker } from "@faker-js/faker";
 
-Cypress.Commands.add("createUsers", (countUser = 0, users = []) => {
+Cypress.Commands.add("createSingleUser", () => {
+  const infoUser = {
+    name: faker.helpers.arrayElement(
+      faker.rawDefinitions.person.first_name.filter((a) => a.length >= 4)
+    ),
+    email: faker.internet.email(),
+  };
+
+  cy.request({
+    method: "POST",
+    url: "https://rarocrud-80bf38b38f1f.herokuapp.com/api/v1/users",
+    body: infoUser,
+  }).then((response) => {
+    return response.body;
+  });
+});
+
+Cypress.Commands.add("createMultipleUsers", (countUser = 0, users = []) => {
   const MINIMUM_USERS = 12;
 
   if (countUser === MINIMUM_USERS) {
